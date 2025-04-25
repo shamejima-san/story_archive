4a5eb9237a80a3d952f68362fac988c3584b537d
shamejima-san <shark.is.sugoi@gmail.com>
2025-04-25 22:09:34 +0900

    main.js

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ const storyForm = document.getElementById("storyForm");
 const storyDetail = document.getElementById("storyDetail");
 const tagFilter = document.getElementById("tagFilter");
 const secretToggle = document.getElementById("secretToggle");
-const NOTION_TOKEN = "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";  // Integrationのトークン
-const DATABASE_ID = "1e04fe9c77ff8014a74ef26d550b8c44";  // データベースのID
 
 let editingStoryId = null;
 let currentFilter = null;
@@ -341,6 +339,9 @@ renderStories(currentFilter);
 renderTimelineView(currentFilter);
 }
 }
+
+const NOTION_TOKEN = "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";  // Integrationのトークン
+const DATABASE_ID = "1e04fe9c77ff8014a74ef26d550b8c44";  // データベースのID
 
 async function saveToNotion(story) {
 const res = await fetch("https://api.notion.com/v1/pages", {

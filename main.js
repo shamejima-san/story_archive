f7f6c86e1cec62a0faa16d0c11e6e79503138c4e
shamejima-san <shark.is.sugoi@gmail.com>
2025-05-02 17:04:38 +0900

    main.js

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ let currentFilter = null;
 let showSecret = false;
 let currentView = "list";
 let sortOrder = "asc";
-renderStories();  
 
 const NOTION_TOKEN = "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";
 const DATABASE_ID = "1e04fe9c77ff805b9575c2213cebb41b";
